90195de9e596e223ed8b31fd6e0e1c1d445d8387
Johan Larsson <johan@johanl.se>
2014-06-05 23:44:28 +0200

    Lagt till AFNetworking

---
+++ Reseplaneraren/Reseplaneraren-Bridging-Header.h
@@ -0,0 +1,9 @@
+//
+//  Reseplaneraren-Bridging-Header.h
+//  Reseplaneraren
+//
+//  Created by Johan Larsson on 2014-06-05.
+//  Copyright (c) 2014 Johan Larsson. All rights reserved.
+//
+
+#import <AFNetworking/AFNetworking.h>
